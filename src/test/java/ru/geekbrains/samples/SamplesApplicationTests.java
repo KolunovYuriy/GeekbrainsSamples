2bb4d2bf3bcf4cbe4fb295cd47634f2fa37785ec
KolunovYuriy <y.kolunov@gmail.com>
2021-07-25 20:59:23 +0300

    prepare

---
+++ src/test/java/ru/geekbrains/samples/SamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.samples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
