c1ac648f6987ca572de3968bed77b9773f283fda
KolunovYuriy <y.kolunov@gmail.com>
2021-07-27 00:31:46 +0300

    homework2 task1 some change

--- src/main/java/ru/geekbrains/samples/lesson2/LessonTwo.java
+++ src/main/java/ru/geekbrains/samples/lesson2/LessonTwo.java
@@ -2,6 +2,7 @@ package ru.geekbrains.samples.lesson2;
 
 public class LessonTwo {
     public static void main(String[] args) {
+        //-task1------------------//
         MyLinkedList sl = new MyLinkedList();
         sl.push(new Cat(4, "Vasya"));
         sl.push(new Cat(3, "Matroskin"));
@@ -16,5 +17,21 @@ public class LessonTwo {
         System.out.println(it.next());
         System.out.println(it.atEnd());
         System.out.println("-----------");
+        //------------------------//
+        DoubleWayLinkedList dwll = new DoubleWayLinkedList();
+        dwll.insertLast(new Cat(5, "Marsel"));
+        dwll.push(new Cat(4, "Vasya"));
+        dwll.push(new Cat(3, "Matroskin"));
+        dwll.push(new Cat(2, "Q"));
+        dwll.push(new Cat(1, "Murzik"));
+        dwll.insertLast(new Cat(6, "Petya"));
+        DoubleWayLinkedList.IteroCat it2 = dwll.iterator();
+        while(it2.hasNext()) {
+            Cat c = it2.next();
+            System.out.println(c);
+        }
+
+        //-task2------------------//
+
     }
 }
